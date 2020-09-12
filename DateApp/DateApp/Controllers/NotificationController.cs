ed24c74679dd5c10554eebbc9f3621544e71d556
Tomasz Krzeminski <koral2323@gmail.com>
2020-09-12 08:57:00 +0200

    Notifications

---
+++ DateApp/DateApp/Controllers/NotificationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DateApp.Controllers
+{
+    public class NotificationController : Controller
+    {
+        public IActionResult CheckNotifty(string Id="Brak")
+        {
+            return View();
+        }
+    }
+}
